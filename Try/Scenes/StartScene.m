635ae72675c02f7c05f41f25c68824244997ca0e
wneo <neo@neotekiMac-mini.local>
2014-04-04 19:06:04 +0800

    fix progress for anchorPoint

--- Try/Scenes/StartScene.m
+++ Try/Scenes/StartScene.m
@@ -18,16 +18,17 @@
 {
 	self = [super initWithSize:size];
 	if (self) {
-		self.anchorPoint = CGPointMake(0.5, 0.5);
+		//self.anchorPoint = CGPointMake(0.5, 0.5);
 		self.background = [SKSpriteNode spriteNodeWithTexture:[SKTexture textureWithImageNamed:@"background"] size:self.frame.size];
-		//self.background = [SKSpriteNode spriteNodeWithColor:[SKColor grayColor] size:self.frame.size];
-		//self.background.blendMode = SKBlendModeReplace;
-		self.background.position = CGPointMake(0.0, 0.0);
+		self.background.position = CGPointMake((0.5 - self.anchorPoint.x)*self.background.size.width,
+											   (0.5 - self.anchorPoint.y)*self.background.size.height);
 		[self addChild:self.background];
 		
 		WYProgressNode *progress = [[WYProgressNode alloc] initWithDoneColor:[SKColor brownColor]
 															  andUndoneColor:[SKColor grayColor]
-																		size:CGSizeMake(100, 20)];
+																		size:CGSizeMake(self.size.width*0.7, 20)];
+		progress.position = CGPointMake((0.5 - self.anchorPoint.x)*self.background.size.width,
+										(0.1 - self.anchorPoint.y)*self.background.size.height);
 		[self addChild:progress];
 		[progress runAction:[SKAction customActionWithDuration:4 actionBlock:^(SKNode *node, CGFloat elapsedTime) {
 			((WYProgressNode *)node).progress = elapsedTime/4;
